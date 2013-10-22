b95ca70156962389fc40d46d780062aa299a6c1f
Joey - Laptop <joey94jk@gmail.com>
2013-10-22 15:48:48 +0200

    Eerste commit - Slim framework toegevoegd - Start pagina toegevoegd

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+	'templates.path' => 'templates',
+	));
+
+$app->get('/hello/:name', 'showName');
+
+function showName($name) {
+	$app = \Slim\Slim::getInstance();
+	$app->render('index.php', array('name' => $name));
+}
+
+$app->run();
---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Start Page</title>
+</head>
+<body>
+
+	<h1>Hello, <?php echo $name; ?></h1>
+
+</body>
+</html>
