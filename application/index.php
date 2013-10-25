72112ca791c2589d2c06b38f6102f06a5bf823af
Trinco Ingels <trincoingels@planet.nl>
2013-10-25 13:51:55 +0200

    uren toevoegen.

--- application/index.php
+++ application/index.php
@@ -14,6 +14,8 @@ $app->get('/student', 'studentPage');
 $app->get('/docent', 'docentPage');
 $app->get('/slc', 'slcPage');
 $app->post('/login', 'loginUser');
+$app->get('/uren', 'urenPage');
+$app->post('/uren', 'addStudielast');
 
 function startPage() {
 	$app = \Slim\Slim::getInstance();
@@ -37,6 +39,22 @@ function docentPage() {
 function slcPage() {
 	$app = \Slim\Slim::getInstance();
 	$app->render('index.php', array('page' => 'SLC Page'));
+}
+
+function urenPage() {
+	$app = \Slim\Slim::getInstance();
+	$app->render('uren.php', array('page' => 'Uren Page'));
+}
+
+function addStudielast() {
+	$db = Database::getInstance();
+	$sql = "INSERT INTO Uren (onderdeel_Id, uren_Date, uren_Studielast, User_user_Id) VALUES (0, :datum, :studielast, 2)";
+	$statement = $db->prepare($sql);
+	//$statement->bindParam("onderdeel", $_POST['onderdeel']);
+	$statement->bindParam('datum', $_POST['date']);
+	$statement->bindParam('studielast', $_POST['studielast']);
+	$statement->execute();
+	$db = null;
 }
 
 function loginUser() {
--- templates/uren.php
+++ templates/uren.php
@@ -14,25 +14,28 @@
 </head>
 <body>
 	
-	<div data-role="header"><h1>Uren Page</h1></div>
+	<div data-role="header"><h1><?php echo $page; ?></h1></div>
 
 	<form data-ajax="false" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 		<!-- Veld voor het onderdeel-->
 		<label for="select-choice-0" class="select">Onderdeel:</label>
-		<select name="select-choice-0" id="select-choice-0">
-		   <option value="standard">1</option>
-		   <option value="rush">2</option>
-		   <option value="express">3</option>
-		   <option value="overnight">4</option>
+		<select name="onderdeel" id="select-choice-0">
+		   <option value="1">1</option>
+		   <option value="2">2</option>
+		   <option value="3">3</option>
+		   <option value="4">4</option>
 		</select>
 		<!-- Veld voor de datum -->
 		<label for="date">Datum:</label>
-		<input type="date" width=" 30px;name="date" id="date" value=""  />	
+		<input type="date" width=" 30px;" name="date" id="date" value=""  />	
 		
 		<!-- Veld voor studielast -->
 		<label for="basic">Studielast:</label>
 		<input type="text" name="studielast" id="basic" data-mini="true" />
-	</form>
+		
+		<!-- Submit knop -->
+		<input type="submit" value="Opslaan" data-theme="b">
+		</form>
 
 </body>
 </html>
