e5e6f1e9545a4dafed1aeefa5203fb5282e89c6b
Joey - Laptop <joey94jk@gmail.com>
2014-01-21 14:29:03 +0100

    - Rol toevoegen werkt nu, bij nieuwe student toevoegen en het wijzigen ervan

--- application/classes/PartManager/SLCPartManager.php
+++ application/classes/PartManager/SLCPartManager.php
@@ -172,9 +172,6 @@ class SLCPartManager {
 
 	public function addStudentToCourse($id, $courseId) {
 		if(isLogged($id)) {
-
-
-
 			$sql = "INSERT INTO Cursus_has_User (Cursus_Id, User_Id) VALUES (:courseId, :userId)";
 
 			$statement = $this->db->prepare($sql);
@@ -188,145 +185,155 @@ class SLCPartManager {
 	}
 
 	public function addStudent($id) {
-
-
 		if(!empty($_POST)) {	
-			$sql = "INSERT INTO User (user_Name, user_Code, user_Email, user_Pass, user_Klas, Rol_rol_Id) 
-		VALUES (:user_name, :user_code, :user_email, :user_pass, :user_klas, 1)"; // 1 is hardcoded because a student always has a role of 1
-		$statement = $this->db->prepare($sql);
-		$statement->bindParam('user_name', $_POST['studentname']);
-		$statement->bindParam('user_code', $_POST['studentcode']);
-		$statement->bindParam('user_email', $_POST['studentemail']);
-		$statement->bindParam('user_pass', $_POST['studentpassword']);
-		$statement->bindParam('user_klas', $_POST['studentklas']);
-
-		if($statement->execute()) {
-			$this->slim->redirect('/urenregistratie/application/index.php/slc/' . $id);
-		}
-	} else {
-		if(isLogged($id)) {
-			echo $this->twigRenderer->renderTemplate('addstudent.twig', array('id' => $id));
-		} else {
-			echo $this->twigRenderer->renderTemplate('noaccess.twig');
-		}
-	}
-}
-
-public function editStudent($id, $studentId) {
-	if(!empty($_POST)) {
-		if(!empty($_POST['actief'])){
-			$actief = 1;
-		}else{
-			$actief = 0;
-		}
-		$sql = "UPDATE User SET user_Name = :userName, user_Code = :userCode, user_Email = :userEmail, user_Pass = :userPass, user_Klas = :userKlas, actief = :userActief WHERE user_Id = :userID";
-		$statement = $this->db->prepare($sql);
-		$statement->bindParam('userName', $_POST['studentname']);
-		$statement->bindParam('userCode', $_POST['studentcode']);
-		$statement->bindParam('userEmail', $_POST['studentemail']);
-		$statement->bindParam('userPass', $_POST['studentpassword']);
-		$statement->bindParam('userKlas', $_POST['studentklas']);
-		$statement->bindParam('userActief', $actief);
-		$statement->bindParam('userID', $studentId);
-		if($statement->execute()) {
-			$this->slim->redirect('/urenregistratie/application/slc/' . $id);
-		}
-	} else {
-		if(isLogged($id)) {
-			$sql = "SELECT * FROM User WHERE user_Id = :userId";
+			$sql = "INSERT INTO User (user_Name, user_Code, user_Email, user_Pass, user_Klas, Rol_rol_Id, actief) 
+			VALUES (:user_name, :user_code, :user_email, :user_pass, :user_klas, :Rol_rol_Id, 1)";
 			$statement = $this->db->prepare($sql);
-			$statement->bindParam('userId', $studentId);
-			$statement->execute();
-			$student = $statement->fetchAll(\PDO::FETCH_ASSOC);
-			echo $this->twigRenderer->renderTemplate('editstudent.twig', array('studentId' => $studentId, 'id' => $id, 'student' => $student[0]));
+			$statement->bindParam('user_name', $_POST['studentname']);
+			$statement->bindParam('user_code', $_POST['studentcode']);
+			$statement->bindParam('user_email', $_POST['studentemail']);
+			$statement->bindParam('user_pass', $_POST['studentpassword']);
+			$statement->bindParam('user_klas', $_POST['studentklas']);
+			$statement->bindParam('Rol_rol_Id', $_POST['rol']);
+
+			if($statement->execute()) {
+				$this->slim->redirect('/urenregistratie/application/index.php/slc/' . $id);
+			}
 		} else {
-			echo $this->twigRenderer->renderTemplate('noaccess.twig');
-		}
-	}
-}
+			if(isLogged($id)) {
+				$sql = 'SELECT * FROM Rol';
+				$statement = $this->db->prepare($sql);
+				$statement->execute();
+				$rollen = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-public function removeStudent($id, $studentId) {
-	if(!empty($_POST)) {
-		$sql = "UPDATE User SET actief = :actief WHERE user_Id = :studentId";
-		$statement = $this->db->prepare($sql);
-		$statement->bindParam('studentId', $studentId);
-		$statement->bindValue('actief', 0);
-
-		if($statement->execute()) {
-			$this->slim->redirect('/urenregistratie/application/index.php/slc/' . $id);
-		} else {
-			print_r($statement->errorInfo());
-		}
-	} else {
-		if(isLogged($id)) {
-			$sql = "SELECT * FROM User WHERE user_Id = :studentId";
-			$statement = $this->db->prepare($sql);
-			$statement->bindParam('studentId', $studentId);
-			$statement->execute();
-			$results = $statement->fetchAll(\PDO::FETCH_ASSOC);
-
-			echo $this->twigRenderer->renderTemplate('removestudent.twig', array('student' => $results[0], 'id' => $id, 'studentId' => $studentId));
-		} else {
-			echo $this->twigRenderer->renderTemplate('noaccess.twig');
-		}
-	}
-}
-
-public function slcOverzicht($id) {
-	if((isLogged($id))) {
-		$totaaluren = 0;
-		$array = null;
-		if(!empty($_POST)){
-			
-			$statement = $this->db->prepare("SELECT 
-				uren_Id, 
-				SUM(uren_Studielast) as studielast, 
-				(SELECT user_Name FROM User WHERE u.User_user_Id = user_Id) AS user_Name,
-				(SELECT cursus_Name FROM Cursus WHERE cursus_Id IN(SELECT Cursus_cursus_Id FROM Onderdeel WHERE onderdeel_Id = u.Onderdeel_onderdeel_Id)) as cursus, 
-				(SELECT cursus_Id FROM Cursus WHERE cursus_Id IN(SELECT Cursus_cursus_Id FROM Onderdeel WHERE onderdeel_Id = u.Onderdeel_onderdeel_Id)) as cursus_Id,
-				(SELECT SUM(onderdeel_Norm) FROM Onderdeel WHERE Cursus_cursus_Id = o.Cursus_cursus_Id) AS totaleNorm
-				FROM 
-				Uren as u,
-				Onderdeel as o
-				WHERE
-				User_user_Id = " . $_POST['student_Id'] . " 
-				AND
-				u.Onderdeel_onderdeel_Id = o.Onderdeel_Id
-				GROUP BY 
-				cursus"  );
-			$statement->execute();
-			$urenoverzichtData = $statement->fetchAll(\PDO::FETCH_ASSOC);
-			$array = array();
-			$count = 0;
-			foreach($urenoverzichtData as $uren )
-			{
-				$onderdeel_norm = $uren['totaleNorm'];
-				$berekening = ($uren['studielast'] / $onderdeel_norm) * 100;
-				if($berekening > 100)
-				{
-					$berekening = $berekening - 100;
-					$berekening = "<font color=\"red\">".$berekening."%</font> boven";
-				}
-				else{
-					$berekening = 100 - $berekening;
-					$berekening = "<font color=\"green\">".$berekening."%</font> onder";
-				}
-				$totaaluren += $uren['studielast'];
-				$studielast_in_uren = min_naar_uren($uren['studielast']);
-				$array[] = array('uren_Id' => $uren['uren_Id'], 'studielast' => $studielast_in_uren, 'cursus_Id' => $uren['cursus_Id'], 'cursus' => $uren['cursus'], 'onderdeel_Norm' => min_naar_uren($uren['totaleNorm']), 'berekening' => $berekening);
-				$count++;
+				echo $this->twigRenderer->renderTemplate('addstudent.twig', array('id' => $id, 'rollen' => $rollen));
+			} else {
+				echo $this->twigRenderer->renderTemplate('noaccess.twig');
 			}
 		}
-		$urenoverzichtData = null;
-		$sql = "SELECT * FROM User WHERE actief <> 0 AND Rol_rol_Id = 1";
-		$statement = $this->db->prepare($sql);
-		$statement->execute();
-		$students = $statement->fetchAll(\PDO::FETCH_ASSOC);
-		echo $this->twigRenderer->renderTemplate('urenoverzicht_slc.twig', array('id' => $id, 'students' => $students, 'student_Name' => $urenoverzichtData[0]['user_Name'], 'urenoverzichtarray' => $array ));
-	}	
-	else {
-		echo $this->twigRenderer->renderTemplate('noaccess.twig');
 	}
-}
+
+	public function editStudent($id, $studentId) {
+		if(!empty($_POST)) {
+			if(!empty($_POST['actief'])){
+				$actief = 1;
+			}else{
+				$actief = 0;
+			}
+			$sql = "UPDATE User SET user_Name = :userName, user_Code = :userCode, user_Email = :userEmail, user_Pass = :userPass, user_Klas = :userKlas, Rol_rol_Id = :rolId, actief = :userActief WHERE user_Id = :userID";
+			$statement = $this->db->prepare($sql);
+			$statement->bindParam('userName', $_POST['studentname']);
+			$statement->bindParam('userCode', $_POST['studentcode']);
+			$statement->bindParam('userEmail', $_POST['studentemail']);
+			$statement->bindParam('userPass', $_POST['studentpassword']);
+			$statement->bindParam('userKlas', $_POST['studentklas']);
+			$statement->bindParam('rolId', $_POST['rol']);
+			$statement->bindParam('userActief', $actief);
+			$statement->bindParam('userID', $studentId);
+			if($statement->execute()) {
+				$this->slim->redirect('/urenregistratie/application/slc/' . $id);
+			}
+		} else {
+			if(isLogged($id)) {
+				$sql = "SELECT * FROM User WHERE user_Id = :userId";
+				$statement = $this->db->prepare($sql);
+				$statement->bindParam('userId', $studentId);
+				$statement->execute();
+				$student = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+				$sql = "SELECT * FROM Rol";
+				$statement = $this->db->prepare($sql);
+				$statement->execute();
+				$rollen = $statement->fetchAll(\PDO::FETCH_ASSOC);
+				echo $this->twigRenderer->renderTemplate('editstudent.twig', array('studentId' => $studentId, 'rollen' => $rollen,'id' => $id, 'student' => $student[0]));
+			} else {
+				echo $this->twigRenderer->renderTemplate('noaccess.twig');
+			}
+		}
+	}
+
+	public function removeStudent($id, $studentId) {
+		if(!empty($_POST)) {
+			$sql = "UPDATE User SET actief = :actief WHERE user_Id = :studentId";
+			$statement = $this->db->prepare($sql);
+			$statement->bindParam('studentId', $studentId);
+			$statement->bindValue('actief', 0);
+
+			if($statement->execute()) {
+				$this->slim->redirect('/urenregistratie/application/index.php/slc/' . $id);
+			} else {
+				print_r($statement->errorInfo());
+			}
+		} else {
+			if(isLogged($id)) {
+				$sql = "SELECT * FROM User WHERE user_Id = :studentId";
+				$statement = $this->db->prepare($sql);
+				$statement->bindParam('studentId', $studentId);
+				$statement->execute();
+				$results = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+				echo $this->twigRenderer->renderTemplate('removestudent.twig', array('student' => $results[0], 'id' => $id, 'studentId' => $studentId));
+			} else {
+				echo $this->twigRenderer->renderTemplate('noaccess.twig');
+			}
+		}
+	}
+
+	public function slcOverzicht($id) {
+		if((isLogged($id))) {
+			$totaaluren = 0;
+			$array = null;
+			if(!empty($_POST)){
+
+				$statement = $this->db->prepare("SELECT 
+					uren_Id, 
+					SUM(uren_Studielast) as studielast, 
+					(SELECT user_Name FROM User WHERE u.User_user_Id = user_Id) AS user_Name,
+					(SELECT cursus_Name FROM Cursus WHERE cursus_Id IN(SELECT Cursus_cursus_Id FROM Onderdeel WHERE onderdeel_Id = u.Onderdeel_onderdeel_Id)) as cursus, 
+					(SELECT cursus_Id FROM Cursus WHERE cursus_Id IN(SELECT Cursus_cursus_Id FROM Onderdeel WHERE onderdeel_Id = u.Onderdeel_onderdeel_Id)) as cursus_Id,
+					(SELECT SUM(onderdeel_Norm) FROM Onderdeel WHERE Cursus_cursus_Id = o.Cursus_cursus_Id) AS totaleNorm
+					FROM 
+					Uren as u,
+					Onderdeel as o
+					WHERE
+					User_user_Id = " . $_POST['student_Id'] . " 
+					AND
+					u.Onderdeel_onderdeel_Id = o.Onderdeel_Id
+					GROUP BY 
+					cursus"  );
+				$statement->execute();
+				$urenoverzichtData = $statement->fetchAll(\PDO::FETCH_ASSOC);
+				$array = array();
+				$count = 0;
+				foreach($urenoverzichtData as $uren )
+				{
+					$onderdeel_norm = $uren['totaleNorm'];
+					$berekening = ($uren['studielast'] / $onderdeel_norm) * 100;
+					if($berekening > 100)
+					{
+						$berekening = $berekening - 100;
+						$berekening = "<font color=\"red\">".$berekening."%</font> boven";
+					}
+					else{
+						$berekening = 100 - $berekening;
+						$berekening = "<font color=\"green\">".$berekening."%</font> onder";
+					}
+					$totaaluren += $uren['studielast'];
+					$studielast_in_uren = min_naar_uren($uren['studielast']);
+					$array[] = array('uren_Id' => $uren['uren_Id'], 'studielast' => $studielast_in_uren, 'cursus_Id' => $uren['cursus_Id'], 'cursus' => $uren['cursus'], 'onderdeel_Norm' => min_naar_uren($uren['totaleNorm']), 'berekening' => $berekening);
+					$count++;
+				}
+			}
+			$urenoverzichtData = null;
+			$sql = "SELECT * FROM User WHERE actief <> 0 AND Rol_rol_Id = 1";
+			$statement = $this->db->prepare($sql);
+			$statement->execute();
+			$students = $statement->fetchAll(\PDO::FETCH_ASSOC);
+			echo $this->twigRenderer->renderTemplate('urenoverzicht_slc.twig', array('id' => $id, 'students' => $students, 'student_Name' => $urenoverzichtData[0]['user_Name'], 'urenoverzichtarray' => $array ));
+		}	
+		else {
+			echo $this->twigRenderer->renderTemplate('noaccess.twig');
+		}
+	}
 
 }
