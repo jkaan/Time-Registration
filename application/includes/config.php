4e44e909cca940504ba6dcea64e7e3cf1ce64739
Joey - Laptop <joey94jk@gmail.com>
2014-01-24 13:40:12 +0100

    - Terugzetten van config.php

---
+++ application/includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file contains the BASE url that is used for routing and redirecting
+ * 
+ */
+
+if(!defined('BASE')) {
+	DEFINE('BASE', 'http://localhost/urenregistratie/application/index.php');
+}
+
+if(!defined('TEMPLATE_BASE')) {
+	DEFINE('TEMPLATE_BASE', 'http://localhost/urenregistratie/templates');
+}
